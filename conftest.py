ba9f96126e418fb81de2bd2c8c2b67d2bddab8db
zheryulia <86978183+zheryulia@users.noreply.github.com>
2021-10-01 18:34:06 +0300

    Update conftest.py

--- conftest.py
+++ conftest.py
@@ -2,41 +2,39 @@ import pytest
 
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
-
 from webdriver_manager.chrome import ChromeDriverManager
 
+from models.auth import AuthData
 from pages.application import Application
 
 
 @pytest.fixture(scope="session")
 def app(request):
-    base_url = request.config.getoption("--base-url")
+    url = request.config.getoption("--url")
     chrome_options = Options()
     chrome_options.add_argument("--headless")
     fixture = Application(
         webdriver.Chrome(ChromeDriverManager().install(), options=chrome_options),
-        base_url,
+        url,
     )
     yield fixture
     fixture.quit()
 
+@pytest.fixture
+def auth(app, request):
+    login = request.config.getoption("--login")
+    password = request.config.getoption("--password")
+    app.open_auth_page()
+    auth_data = AuthData(login=login, password=password)
+    app.login.auth(auth_data)
+    assert app.login.is_auth(), "Вход не выполнен"
+    yield
+    app.login.sign_out()
 
 def pytest_addoption(parser):
     parser.addoption(
-        "--base-url",
+        "--url",
         action="store",
         default="https://qacoursemoodle.innopolis.university",
-        help="enter base_url",
-    ),
-    parser.addoption(
-        "--username",
-        action="store",
-        default="yuliazher",
-        help="enter username",
-    ),
-    parser.addoption(
-        "--password",
-        action="store",
-        default="Yul343!!",
-        help="enter password",
-    ),
+        help="ссылка на qacourse",
+    )
